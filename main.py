051cfccf9b82aeeeadf54ee2e65a382a73ccb1ef
ohiokas <147150341+ohiokas@users.noreply.github.com>
2023-11-16 23:08:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 def turn_right():
     for i in range(3):
         turn_left()
-
-
-
 while not at_goal():
-        if right_is_clear():
-               turn_right()
-
-       
+    if right_is_clear():
+        turn_right()
         move()
     elif front_is_clear():
         move()   
